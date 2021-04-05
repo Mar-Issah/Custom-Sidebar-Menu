b123ce4c454cb0699b3d49eff2dda6509d72d0a6
Siya <67198163+Mar-Issah@users.noreply.github.com>
2021-04-05 13:55:12 -0400

    Update index.js file

--- index.js
+++ index.js
@@ -3,20 +3,11 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", () => {
-    // using the long method, if sidebar has class show-sidebar remove it else add it
-
-//     if (sidebar.classList.contains("show-sidebar")) {
-//         sidebar.classList.remove("show-sidebar");
-
-//     } else {
-//         sidebar.classList.add("show-sidebar");
-// }
-    
-    //using the short and effective toggle()method
+   
     sidebar.classList.toggle("show-sidebar");
 });
 
-//for the close button when you click on it it removes the show-sidebar class which does not show the sidebar
+
 closeBtn.addEventListener("click", () => { 
     sidebar.classList.remove("show-sidebar");
 });
